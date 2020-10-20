7c948094be557b8e54169211dca1c6d8d8160e77
AndrePelealu <andre02.9d@gmail.com>
2020-10-20 23:06:52 +0700

    update route data pribadi

--- routes/api.php
+++ routes/api.php
@@ -72,7 +72,7 @@ Route::post('perusahaan/logout', 'UserPerusahaanController@logout');//checked
 	/* DATA PRIBADI */
 
 
-Route::post('data-pribadi/'		,'DataPribadiController@PostDataPribadi');//checked
+Route::post('data-pribadi/post'		,'DataPribadiController@PostDataPribadi');//checked
 Route::get('data-pribadi'		,'DataPribadiController@GetDataPribadi');//checked
 Route::get('data-pribadi/{id}'	,'DataPribadiController@GetDataPribadiById');//checked
 Route::put('data-pribadi/{id}'	,'DataPribadiController@UpdateDataPribadi');//checked
